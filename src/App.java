b39df6c8c29e49629c497fb0fa17c8f728368655
Relate13 <littleflyingfish@sina.com>
2022-11-15 20:40:21 +0800

    Init

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Terminal t=new Terminal();
+        t.StartListening();
+        /*ProgramCompareTool t = new ProgramCompareTool();
+        t.SetRoot("/home/relate13/Desktop/input/");
+        t.StartCompareRoutine();*/
+        //t.CompareTask("/home/relate13/Desktop/input/4A/");
+    }
+}
